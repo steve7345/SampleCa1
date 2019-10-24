1e2423169383576c8b8074835d951a38d335c082
s00189972 <s00189972@mail.itsligo.ie>
2019-10-24 11:43:52 +0100

    Add project files.

---
+++ SampleCa1/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCa1
+{
+    public class Player
+    {
+        public int ID { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public int Score { get; set; }
+    }
+}
